f62075a9394b04cf121367226668c165c36f3875
CYOSP <cyosp@users.noreply.github.com>
2017-03-24 22:04:18 +0100

    Allow to store sensor data

---
+++ software/var/www/html/cyths/API/set/sensor/data/index.php
@@ -0,0 +1,121 @@
+<?php
+
+//
+// Author: CYOSP
+// Version: 1.0.0
+//
+// Post arguments:
+//  - <sensorId> : Sensor identifier (1->127)
+//  - <date> : Sensor data receiving date (format: YYYY-MM-DD)
+//  - <time> : Sensor data receiving time (format: HH-MM-SS.NNN)
+//  - <temperature> : Sensor temperature (-40.0->62.4)
+//  - <humidity> : Sensor humidity (0.0->100.0)
+
+//
+// 2017-03-24 V 1.0.0
+//  - First release
+//
+
+// TEST
+//  * curl -i --silent --data "sensorId=123&date=2017-03-24&time=22:00:00.000&temperature=20.0&humidity=60.0" "http://localhost/cyths/API/set/sensor/data/"
+
+// Get parameters
+$sensorId = $_POST['sensorId'];
+$date = $_POST['date'];
+$time = $_POST['time'];
+$temperature = $_POST['temperature'];
+$humidity = $_POST['humidity'];
+
+// Check input arguments
+if( $sensorId != "" && $date != "" && $time != "" && $temperature != "" && $humidity != "" )
+{
+	//
+	// Update JSON configuration file
+	//
+	
+	// Get a file pointer to the lock file
+	$fp = fopen( "/tmp/cyths.config.sync.lock" , "c" );
+	// Lock file for synchronisation
+	if( flock( $fp , LOCK_EX ) )
+	{	
+		// Define path of file to update
+		$configFile="../../../../data/config.json";
+		
+		// Get and parse JSON file
+		$data = json_decode( file_get_contents( $configFile ) , true );
+
+		// Pause 5 seconds to prove that race condition doesn't exist
+		//sleep( 5 );
+
+		//
+		// Define data to add/update
+		//
+		$sensorData['date'] = $date;
+		$sensorData['time'] = $time;
+		$sensorData['temperature'] = $temperature;
+		$sensorData['humidity'] = $humidity;
+
+		//
+		// Search entry to update
+		//
+		$found = false;
+		foreach( $data['switchesList'] as $key => $entry )
+		{
+			// Entry to update found
+			if( $entry['sensor']['id'] == $sensorId )
+			{
+				// Sensor found
+				$found = true;
+
+				// Add/update data
+				$sensor['id'] = $sensorId;
+				$sensor['data'] = $sensorData;
+				$data['switchesList'][$key]['sensor'] = $sensor;
+			}
+		}
+
+		// Add an entry if no one found
+		if( ! $found )
+		{
+			// Sub level
+			$sensor['id'] = $sensorId;
+			$sensor['data'] = $sensorData;
+			// Main level
+			$newValue['label'] = "Unknown";
+			$newValue['channel'] = "";
+			$newValue['rcId'] = "";
+			$newValue['state'] = "unknown";
+			$newValue['sensor'] = $sensor;
+			$newValue['info'] = "";
+
+			// Add new entry
+			array_push( $data['switchesList'] , $newValue );
+		}
+
+		// Update JSON file
+		file_put_contents( $configFile , json_encode( $data , JSON_PRETTY_PRINT ) );
+
+		// Release lock file
+		flock( $fp , LOCK_UN );
+		// Close the lock file
+		fclose( $fp );
+	}
+	else
+	{
+		$json['result'] = "error";
+		$json['message'] = "Fail to lock synchronisation file";
+	}
+
+	$json['result'] = "";
+	$json['message'] = "";
+	
+}
+else
+{
+	$json['result'] = "error";
+	$json['message'] = "Invalid argument(s)";
+}
+
+echo json_encode( $json );
+
+?>
